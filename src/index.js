657fba45b0a86fffd46ffaf8762fd7474443845a
Pratik4230 <pratikjadhav1438@gmail.com>
2024-11-20 12:10:03 +0530

    minor change

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
 import app from "./app.js";
-import 'dotenv/config';
+import "dotenv/config";
 import connectDB from "./DB/DB.js";
-
 
 const PORT = process.env.PORT;
 
 connectDB()
-.then(() => {
+  .then(() => {
     console.log("Connected to DB");
     app.listen(PORT, () => {
-        console.log(`server is running on port ${PORT}`);
-        
-    }  )
-} 
-)
-.catch((error) => {
+      console.log(`server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
     console.log("connectDB failed : ", error);
-    
-})
-
+  });
